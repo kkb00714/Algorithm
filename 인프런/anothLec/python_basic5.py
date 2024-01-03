49c5218bdacafaa7fd3899ed4e0339c58ecaf6fe
kkb <0furiousblade0@gmail.com>
2024-01-03 14:39:12 +0900

    [#10] Python practice (6)

---
+++ 인프런/anothLec/python_basic5.py
@@ -0,0 +1,25 @@
+# 6. 중첩 반복문(2중 for문)
+
+# for i in range(5):
+#     print("i: ", i, sep='', end=' ')
+#     for j in range(5):
+#         print('j: ', j, sep='', end=' ')
+#     print()
+
+
+# 별
+# for i in range(5):
+#     for j in range(i + 1):
+#         print("*", end=' ')
+#     print()
+
+# 역별
+# for i in range(5):
+#     for j in range(5 - i):
+#         print("*", end=' ')
+#     print()
+    
+    
+# 다이아몬드 도전
+
+
